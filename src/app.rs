9421c32750a0f329e32005b0b45c944924a7ccdb
idaho06 <cesar.idaho@gmail.com>
2022-05-12 22:11:17 +0200

    Changed name of crate following README

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,7 @@
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
+pub struct RaytracerApp {
     // Example stuff:
     label: String,
 
@@ -10,17 +10,17 @@ pub struct TemplateApp {
     value: f32,
 }
 
-impl Default for TemplateApp {
+impl Default for RaytracerApp {
     fn default() -> Self {
         Self {
             // Example stuff:
-            label: "Hello World!".to_owned(),
+            label: String::from("Hello, Akino!"),
             value: 2.7,
         }
     }
 }
 
-impl TemplateApp {
+impl RaytracerApp {
     /// Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // This is also where you can customized the look at feel of egui using
@@ -36,7 +36,7 @@ impl TemplateApp {
     }
 }
 
-impl eframe::App for TemplateApp {
+impl eframe::App for RaytracerApp {
     /// Called by the frame work to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, self);
