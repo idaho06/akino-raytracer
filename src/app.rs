2087eb2a29a59d577723191c1db15ae94aa036b2
idaho06 <cesar.idaho@gmail.com>
2022-05-18 07:39:49 +0200

    Layout setup and readme cleanup

--- src/app.rs
+++ src/app.rs
@@ -1,13 +1,24 @@
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
+/*
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
+*/
 pub struct RaytracerApp {
     // Example stuff:
     label: String,
 
     // this how you opt-out of serialization of a member
-    #[serde(skip)]
+    //#[serde(skip)]
     value: f32,
+    //#[serde(skip)]
+    render_result: Option<egui::TextureHandle>,
+
+    // pixel values
+    red: u8,
+    green: u8,
+    blue: u8,
+    pixelx: usize,
+    pixely: usize,
 }
 
 impl Default for RaytracerApp {
@@ -16,6 +27,12 @@ impl Default for RaytracerApp {
             // Example stuff:
             label: String::from("Hello, Akino!"),
             value: 2.7,
+            render_result: None,
+            red: 255,
+            green: 255,
+            blue: 255,
+            pixelx: 0,
+            pixely: 0,
         }
     }
 }
@@ -25,27 +42,38 @@ impl RaytracerApp {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // This is also where you can customized the look at feel of egui using
         // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
+        cc.egui_ctx.set_visuals(egui::Visuals::dark());
 
         // Load previous app state (if any).
         // Note that you must enable the `persistence` feature for this to work.
+        /*
         if let Some(storage) = cc.storage {
             return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
         }
-
+        */
         Default::default()
     }
 }
 
 impl eframe::App for RaytracerApp {
     /// Called by the frame work to save state before shutdown.
-    fn save(&mut self, storage: &mut dyn eframe::Storage) {
-        eframe::set_value(storage, eframe::APP_KEY, self);
+    fn save(&mut self, _storage: &mut dyn eframe::Storage) {
+        //eframe::set_value(storage, eframe::APP_KEY, self);
     }
 
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        let Self { label, value } = self;
+        let Self {
+            label,
+            value,
+            render_result,
+            red,
+            green,
+            blue,
+            pixelx,
+            pixely,
+        } = self; // This removes the need to write "self.whatever" everytime. And apparently everything is mut
 
         // Examples of how to create different panels and windows.
         // Pick whichever suits you.
@@ -64,25 +92,49 @@ impl eframe::App for RaytracerApp {
         });
 
         egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Side Panel");
+            //ui.heading("Side Panel");
 
+            /*
             ui.horizontal(|ui| {
                 ui.label("Write something: ");
                 ui.text_edit_singleline(label);
             });
-
+            */
+            /*
             ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
             if ui.button("Increment").clicked() {
                 *value += 1.0;
             }
+            */
+
+            ui.add(egui::Slider::new(red, 0..=255).text("Red"));
+            ui.add(egui::Slider::new(green, 0..=255).text("Green"));
+            ui.add(egui::Slider::new(blue, 0..=255).text("Blue"));
+            ui.horizontal(|ui| {
+                ui.label("x: ");
+                ui.add(egui::DragValue::new(pixelx));
+            });
+            ui.horizontal(|ui| {
+                ui.label("y: ");
+                ui.add(egui::DragValue::new(pixely));
+            });
+
+            ui.horizontal(|ui| {
+                if ui.button("Add pixel").clicked() {
+                    //
+                }
+                if ui.button("Set background").clicked() {
+                    //
+                }
+            });
 
             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
                 ui.horizontal(|ui| {
                     ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
-                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-                    ui.label(" and ");
-                    ui.hyperlink_to("eframe", "https://github.com/emilk/egui/tree/master/eframe");
+                    ui.label("made by ");
+                    ui.hyperlink_to("@idaho06", "https://github.com/idaho06");
+                    ui.label(" from ");
+                    ui.hyperlink_to("AkinoSoft", "https://akinosoft.itch.io/");
                 });
             });
         });
@@ -90,22 +142,27 @@ impl eframe::App for RaytracerApp {
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
 
-            ui.heading("eframe template");
+            ui.heading("Render result");
+            /*
             ui.hyperlink("https://github.com/emilk/eframe_template");
             ui.add(egui::github_link_file!(
                 "https://github.com/emilk/eframe_template/blob/master/",
                 "Source code."
             ));
+            */
             egui::warn_if_debug_build(ui);
         });
 
-        if false {
-            egui::Window::new("Window").show(ctx, |ui| {
-                ui.label("Windows can be moved by dragging them.");
-                ui.label("They are automatically sized based on contents.");
-                ui.label("You can turn on resizing and scrolling if you like.");
-                ui.label("You would normally chose either panels OR windows.");
-            });
-        }
+        /*
+        egui::Window::new("Render result")
+        .resizable(true)
+        .scroll2([true,true])
+        .show(ctx, |ui| {
+            ui.label("Windows can be moved by dragging them.");
+            ui.label("They are automatically sized based on contents.");
+            ui.label("You can turn on resizing and scrolling if you like.");
+            ui.label("You would normally chose either panels OR windows.");
+        });
+        */
     }
 }
